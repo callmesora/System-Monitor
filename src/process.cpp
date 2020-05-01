ee6792fb81f5baf50d045a46bb60c8f2cd56c7ae
Pedro <pedroazevedo6@gmail.com>
2020-05-01 01:07:24 +0100

    changedproccess_CPUUtilization

--- src/process.cpp
+++ src/process.cpp
@@ -6,6 +6,7 @@
 #include "linux_parser.h"
 #include "process.h"
 #include "processor.h"
+#include <iostream>
 
 using std::string;
 using std::to_string;
@@ -15,16 +16,57 @@ using std::vector;
 int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { 
+float Process::CpuUtilization() { /*
   long activejiffies = LinuxParser::ActiveJiffies(pid_);
   long processuptime= LinuxParser::UpTime(pid_);
   long systemuptime= LinuxParser::UpTime();
   long seconds= systemuptime - processuptime ;
+ 
 
-  
-  return static_cast<float>(activejiffies/seconds); } //Ask Mentor is this is how static_cast works
+  if (seconds != 0){
+  return (activejiffies/seconds); }
+   //Ask Mentor is this is how static_cast works
   //https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
 
+  else {
+    return 0.0;
+  }
+  /**/
+  
+  std::string line, value;
+  std::vector<string> utilization;
+  std::ifstream filestream(LinuxParser::kProcDirectory + to_string(pid_) + LinuxParser::kStatFilename);
+
+  if (filestream.is_open()) {
+    std::getline(filestream, line);
+    std::istringstream linestream(line);
+
+    int count = 0;
+    while (count < 22) {
+      linestream >> value;
+      utilization.push_back(value);
+      count ++;
+    }
+  }
+
+  float utime = stof(utilization[13]);
+  float stime = stof(utilization[14]);
+  float cutime = stof(utilization[15]);
+  float cstime = stof(utilization[16]);
+  float starttime = stof(utilization[21]);
+
+  float Hertz = sysconf(_SC_CLK_TCK);
+  float uptime = LinuxParser::UpTime();
+
+  float total_time = utime + stime + cutime + cstime;
+  float seconds = uptime - (starttime / Hertz);
+
+  float cpu_usage = ((total_time / Hertz) / seconds);
+
+  //for some reason the first way didn't work, can't work with longs aparently?
+
+  return cpu_usage;
+  }
 // TODO: Return the command that generated this process
 string Process::Command() { return LinuxParser::Command(pid_); }
 
